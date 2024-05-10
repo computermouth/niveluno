fe0c383f80f24d663dd3eefcfe48424f3a5b8070
computermouth <computermouth@crunchbangplusplus.org>
2024-05-09 21:43:04 -0600

    light test and time experiments

--- src/main.rs
+++ src/main.rs
@@ -109,6 +109,11 @@ fn main() -> Result<(), String> {
     // todo, run this if r_buffer usage has changed
     render::submit_buffer()?;
 
+    let start_ms = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .map_err(|e| NUError::SystemTimeError(e.to_string()))?
+        .as_millis();
+
     let mut time = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .map_err(|e| NUError::SystemTimeError(e.to_string()))?
@@ -137,22 +142,30 @@ fn main() -> Result<(), String> {
 
         render::prepare_frame()?;
 
-        let dc = render::DrawCall {
-            pos: Vec3 {
-                x: 0.,
-                y: 0.,
-                z: 0.,
-            },
-            yaw: 0.0,
-            pitch: 0.0,
-            texture: tex as u32,
-            f1: b as i32,
-            f2: b as i32,
-            mix: 0.0,
-            num_verts: 36,
-            unlit: true,
-        };
-        draw(dc)?;
+        for i in 0..10 {
+            let dc = render::DrawCall {
+                pos: Vec3 {
+                    x: i as f32 * -18.,
+                    y: i as f32 * -18.,
+                    z: i as f32 * 36.,
+                },
+                yaw: 0.0,
+                pitch: 0.0,
+                texture: tex as u32,
+                f1: b as i32,
+                f2: b as i32,
+                mix: 0.0,
+                num_verts: 36,
+                unlit: false,
+            };
+            draw(dc)?;
+        }
+
+        let ms = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .map_err(|e| NUError::SystemTimeError(e.to_string()))?
+        .as_millis();
+        render::push_light(Vec3{x: 25., y: 25., z: 25. + 100. * (0.001 * (ms - start_ms) as f32).sin() }, 15., 100., 150., 50.)?;
 
         text::push_surface(&title)?;
 
