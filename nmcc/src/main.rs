9ed47d6647a842e66c9b225fa4279a6d32a98b92
computermouth <computermouth@crunchbangplusplus.org>
2024-05-14 21:49:13 -0600

    match on extras

--- nmcc/src/main.rs
+++ nmcc/src/main.rs
@@ -1,13 +1,16 @@
 
+use std::vec;
+
 use gltf;
 use serde::{Deserialize, Serialize};
-use rmp_serde::{Deserializer, Serializer};
+use serde_json;
+use rmp_serde;
 
 #[derive(Debug)]
 struct NmccError<'a>(&'a str);
 impl<'a> std::fmt::Display for NmccError<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "NmccError: {:?}", self.0)
     }
 }
 impl<'a> std::error::Error for NmccError<'a> {}
@@ -90,6 +93,100 @@ struct Map {
     pub inst: Vec<Instance>
 }
 
+struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+struct Quat4 {
+    x: f32,
+    y: f32,
+    z: f32,
+    w: f32,
+}
+
+// wow this fuckin sucks
+fn trs_from_decomp (i: ([f32; 3], [f32; 4], [f32; 3])) -> (Vec3, Quat4, Vec3) {
+    (
+        Vec3 {
+            x: i.0[0],
+            y: i.0[1],
+            z: i.0[2],
+        },
+        Quat4 {
+            x: i.1[0],
+            y: i.1[1],
+            z: i.1[2],
+            w: i.1[3],
+        },
+        Vec3 {
+            x: i.2[0],
+            y: i.2[1],
+            z: i.2[2],
+        }
+    )
+}
+
+fn get_ref_obj(n: gltf::Node) -> Option<Reference> {
+
+    let (c_pos, _, scale) = trs_from_decomp(n.transform().decomposed());
+
+    if c_pos.x >= 0. ||
+       c_pos.y >= 0. ||
+       c_pos.z >= 0. {
+        return None;
+    }
+
+    if scale.x <= 0. ||
+       scale.y <= 0. ||
+       scale.z <= 0. {
+        eprint!("W: scale was negative, skipping {:?}", n.name());
+        return None;
+    }
+
+    #[derive(Deserialize)]
+    struct Extras<'a> {
+        _type: Option<&'a str>,
+        _entity: Option<&'a str>
+    }
+
+    let extras: Option<Extras>;
+    let jstr: String;
+
+    if let Some(json_raw) = n.extras() {
+        jstr = json_raw.to_string();
+        if let Ok(e) = serde_json::from_str(&jstr) {
+            extras = e;
+        } else {
+            return None;
+        }
+    } else {
+        eprint!("W: node in reference zone, with no extras {:?}", n.name());
+        return None;
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    enum EntityInstance {
+        General(GeneralInstance),
+        Player(PlayerInstance),
+        Light(LightInstance),
+        Trigger(TriggerInstance),
+    }
+
+    match extras {
+        Some(Extras{_type: Some("decor"), ..}) => None, // do decor
+        Some(Extras{_type: Some("entity"), _entity: Some("player")}) => None, // do entity - player
+        Some(Extras{_type: Some("entity"), _entity: Some("light")}) => None, // do entity - light
+        Some(Extras{_type: Some("entity"), _entity: Some("trigger")}) => None, // do entity - trigger
+        Some(Extras{_type: Some("entity"), _entity: Some(_)}) => None, // do entity - general
+        Some(Extras{_type: Some(_), ..}) => None, // warn, unknown type
+        Some(Extras{_type: None, ..}) => None, // warn, no type
+        None => None, // warn no extras
+    }
+
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args: Vec<String> = std::env::args().collect();
@@ -102,9 +199,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let (document, buffers, images) = gltf::import(path)?;
 
-    for node in document.nodes() { }
+    let map = Map{ refs: vec![], inst: vec![] };
 
+    // wasteful looping over it twice.
+    // pack refs
+    let skip_refs: Vec<usize> = vec![];
+    for (i , node) in document.nodes().enumerate() {
+        if let Some(r) = get_ref_obj(node) {}
+    }
 
+    /*
     let m = Map {
         refs: vec![
             Reference::Decor(DecorReference { vertex_count: 2, vertices: vec![1., 2.], u: vec![3., 4.], v: vec![5., 6.], texture: vec![7, 8] }),
@@ -122,17 +226,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     let mut buf = Vec::new();
-    m.serialize(&mut Serializer::new(&mut buf))?;
+    m.serialize(&mut rmp_serde::Serializer::new(&mut buf))?;
 
     std::fs::write("map.mp", buf)?;
 
     let i = std::fs::read("map.mp")?;
 
-    let mut de = Deserializer::new(std::io::Cursor::new(&i[..]));
+    let mut de = rmp_serde::Deserializer::new(std::io::Cursor::new(&i[..]));
     let o: Map = Deserialize::deserialize(&mut de)?;
-
-    eprintln!("{:?}", m);
-    eprintln!("{:?}", o);
+    */
 
     Ok(())
 }
