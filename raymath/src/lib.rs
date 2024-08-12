8ef412d56709bb478d41e943670e7e559aa3150f
computermouth <computermouth@crunchbangplusplus.org>
2024-08-12 01:33:00 -0600

    debug traits

--- raymath/src/lib.rs
+++ raymath/src/lib.rs
@@ -14,7 +14,15 @@ pub struct Vector2 {
     pub x: f32,
     pub y: f32,
 }
-#[derive(Copy, Clone)]
+
+// todo -- deleteme
+impl From<[f32; 3]> for Vector2 {
+    fn from(f: [f32; 3]) -> Self {
+        Vector2{x: f[0], y: f[1]}
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Vector3 {
     pub x: f32,
     pub y: f32,
@@ -40,6 +48,7 @@ pub struct Vector4 {
     pub z: f32,
     pub w: f32,
 }
+
 pub type Quaternion = Vector4;
 #[derive(Copy, Clone)]
 pub struct Matrix {
@@ -60,10 +69,35 @@ pub struct Matrix {
     pub m11: f32,
     pub m15: f32,
 }
+
+impl From<[f32; 16]> for Matrix {
+    fn from(f: [f32; 16]) -> Self {
+        Matrix{
+            m0: f[0],
+            m1: f[1],
+            m2: f[2],
+            m3: f[3],
+            m4: f[4],
+            m5: f[5],
+            m6: f[6],
+            m7: f[7],
+            m8: f[8],
+            m9: f[9],
+            m10: f[10],
+            m11: f[11],
+            m12: f[12],
+            m13: f[13],
+            m14: f[14],
+            m15: f[15],
+        }
+    }
+}
+
 #[derive(Copy, Clone)]
 pub struct Float3 {
     pub v: [f32; 3],
 }
+
 #[derive(Copy, Clone)]
 pub struct Float16 {
     pub v: [f32; 16],
